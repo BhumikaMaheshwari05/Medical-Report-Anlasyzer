414c8375beb8b3542294200ad06fd5b6e3de3ccf
Avinash Lodhi <avinashlodhi58@gmail.com>
2024-09-22 13:19:43 +0530

    Login Authentication done and showing the same dashboard to all the users which need to be changes

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,5 @@
-Thanks for downloading this template!
-
-Template Name: Selecao
-Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
+A medical report analyzer is a software tool or system designed to automatically interpret, 
+organize, and summarize the vast and complex information found in medical reports. 
+These tools often use natural language processing (NLP) and machine learning to 
+extract relevant clinical data from unstructured text. Here's a breakdown of key 
+functions and features of a medical report analyzer:
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask,render_template,redirect,request
+from flask import Flask,render_template,redirect,request,session
 from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
 
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
-#app.secret_key = 'super-secret-key'
+app.secret_key = 'super-secret-key'
 
 
 app.config['SQLALCHEMY_DATABASE_URI']="mysql+mysqlconnector://root:@localhost/medrep"
 
 db = SQLAlchemy(app) 
+
+bcrypt=Bcrypt(app)
 
 class Prevreports(db.Model):
     table_name='prevreports'
@@ -31,22 +35,47 @@ class Contact(db.Model):
     
 
 
+class Login(db.Model):
+    table_name='login'
+    Sno=db.Column(db.Integer,primary_key=True)
+    Username=db.Column(db.String(80),nullable=False)
+    Email=db.Column(db.String(80),nullable=False)
+    Password=db.Column(db.String(150),nullable=False)
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def log():
+    if request.method=="POST":
+        usr=request.form.get("usr")
+        passw=request.form.get("password")
+        user=Login.query.filter_by(Username=usr).first()
+        if user:
+            if bcrypt.check_password_hash(user.Password,passw):
+                session['user']=usr
+                return redirect("/dashboard") 
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register",methods=["GET","POST"])
 def reg():
+    if request.method=="POST":
+        usr=request.form.get("usr")
+        email=request.form.get("email")
+        passw=bcrypt.generate_password_hash(request.form.get("password"))
+        entry=Login(Username=usr,Email=email,Password=passw)
+        db.session.add(entry)
+        db.session.commit()
+        return redirect("/login")
     return render_template("register.html")
 
 
 @app.route("/forget")
 def hui():
+    
     return render_template("forget.html")
 
 @app.route("/contact",methods=["GET","POST"])
@@ -65,8 +94,19 @@ def contact():
 
 @app.route("/dashboard")
 def dash():
-    data=Prevreports.query.filter_by().all()
-    return render_template("indexdsh.html",data=data)
+    if('user' in session):
+        user=Login.query.filter_by(Username=session['user']).first()
+        if user:
+            data=Prevreports.query.filter_by().all()
+            return render_template("indexdsh.html",data=data)
+    else:
+        return redirect('/login')
+    #return render_template("indexdsh.html",data=data)
+    
+@app.route("/logout")
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
